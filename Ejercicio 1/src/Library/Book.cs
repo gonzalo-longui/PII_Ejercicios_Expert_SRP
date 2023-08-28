c6a83b058104c6bd9766c36d3743f2bfeadb365a
longuigon <longuigon+git@gmail.com>
2023-08-28 19:20:50 -0300

    Ejercicio 1 con comentarios

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -8,7 +8,14 @@ namespace SRP
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
+        /*
+        Expert: el libro no tiene por qué saber en qué sector o shelf está.
+        SRP: el libro no tiene la responsabilidad de saber esas cosas
 
+        Se crean clases Sector y Shelf con sus respectivos métodos para efectos similares al anterior método ShelveBook
+        Se crea LibraryManager, que tiene una lista estática de todos los sectores, y un método FindBookLocation para localizar un objeto Book
+        en sector/shelf
+        */
         public Book(String title, String author, String code)
         {
             this.Title = title;
